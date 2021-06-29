5b505da561fe66ca2326243a735286a821fc0c7e
TuanNguyen <ndtuan.bkdn@gmail.com>
2021-06-29 21:40:14 +0700

    bt_buoi1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import FunctionComponent from "./components/FunctionComponent";
 // import ClassComponent from "./components/ClassComponent";
 // import HomeComponent from "./components/BaiTapLayoutComponent/HomeComponent";
-import Databinding from "./Databinding/Databinding";
+// import Databinding from "./Databinding/Databinding";
+import BaiTapThucHanhLayout from "./components/BaiTapThucHanhLayout/BaiTapThucHanhLayout";
 function App() {
   return (
     <div className="App">
       {/* <HomeComponent /> */}
-      <Databinding />
+      {/* <Databinding /> */}
+      <BaiTapThucHanhLayout />
     </div>
   );
 }
---
+++ src/components/BaiTapThucHanhLayout/BodyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import BannerComponent from './BannerComponent'
+import ItemComponent from './ItemComponent'
+export default class BodyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <BannerComponent />
+                <ItemComponent />
+            </div>
+        )
+    }
+}
