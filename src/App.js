25b46ffe693b2f9f2181a987d68ab468a65a9e4a
TuanNguyen <ndtuan.bkdn@gmail.com>
2021-07-18 19:23:45 +0700

    BTThuKinh

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import FunctionComponent from "./Components/FunctionComponent";
+// import ClassComponent from "./Components/ClassComponent";
+// import HomeComponent from "./Components/BaiTapLayoutComponent/HomeComponent";
+// import Databinding from "./Databinding/Databinding"
+// import BaiTapThucHanhLayout from "./Components/BaiTapThucHanhLayout/BaiTapThucHanhLayout";
+// import Demo from "./Databinding/Demo";
+// import HandleEvent from "./HandleEvent/HandleEvent";
+// import StyleComponent from "./StyleComponent/StyleComponent";
+import BaiTapThuKinh from "./components/BaiTapThuKinh/BaiTapThuKinh";
+
+// import StateDemo from "./StateDemo/StateDemo"
+function App() {
+  return (
+    <div className="App">
+          {/* <HomeComponent /> */}
+          {/* <Databinding /> */}
+          {/* <BaiTapThucHanhLayout /> */}
+          {/* <Demo />  */}
+          {/* <HandleEvent></HandleEvent> */}
+
+          {/* <StyleComponent /> */}
+          {/* <p className="fontBold">ahihi</p> */}
+          {/* <StateDemo /> */}
+          <BaiTapThuKinh />
+          
+    </div>
+  );
+}
+
+export default App;
