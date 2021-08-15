72200088e245d876104c1bb9ab36b9c4a2ee11ef
TuanNguyen <ndtuan.bkdn@gmail.com>
2021-08-15 19:37:15 +0700

    react_redux_bt_form

--- src/DemoRedux/BaiTapForm/Form.js
+++ src/DemoRedux/BaiTapForm/Form.js
@@ -65,18 +65,21 @@ class Form extends Component {
         // cản sự kiện submit
         event.preventDefault();
         let valid = true;
-        for(let key in this.state.errors) {
-            if(this.state.errors[key] !== ''){
+        for(let key in this.props.sinhVienChinhSuaLoadLai.errors) {
+            if(this.props.sinhVienChinhSuaLoadLai.errors[key] !== ''){
                 valid = false;
+                console.log("Đã Vào Đây")
                 break
             }
         }
-        for(let key in this.state.value) {
-            if(this.state.value[key] === ''){
+        for(let key in this.props.sinhVienChinhSuaLoadLai.value) {
+            if(this.props.sinhVienChinhSuaLoadLai.value[key] === ''){
                 valid = false;
+                console.log("Đã Vào Đây 2")
                 break
             }
         }
+         // cái khúc này a đang sai cái gì này 
         if(!valid) {
             alert('Dữ liệu không hợp lệ')
             return;
@@ -84,13 +87,13 @@ class Form extends Component {
         //submit lên redux
         const action = {
             type: 'THEM_SINH_VIEN',
-            sinhVien: this.state.value
+            sinhVien: this.props.sinhVienChinhSuaLoadLai.value
         }
         this.props.dispatch(action)
     }
 
     render() {
-        let {maSinhVien,email,hoTen,soDienThoai} = this.props.sinhVienChinhSua;
+        let {maSinhVien,email,hoTen,soDienThoai} = this.props.sinhVienChinhSuaLoadLai.value;
 
 
 
@@ -105,30 +108,37 @@ class Form extends Component {
                         <div className='form-group'>
                             <p>Mã Sinh Viên</p>
                             <input value={maSinhVien} className='form-control' name='maSinhVien' onChange={this.handleChangeInput}/>
-                            <p className='text-danger'>{this.state.errors.maSinhVien}</p>
+                            <p className='text-danger'>{this.props.sinhVienChinhSuaLoadLai.errors.maSinhVien}</p>
                         </div>
                         <div className='form-group'>
                             <p>Email</p>
                             <input value={email} typeEmail='email' className='form-control' name='email' onChange={this.handleChangeInput}/>
-                            <p className='text-danger'>{this.state.errors.email}</p>
+                            <p className='text-danger'>{this.props.sinhVienChinhSuaLoadLai.errors.email}</p>
                         </div>
                     </div>
                     <div className='col-6'>
                     <div className='form-group'>
                             <p>Số Điện Thoại</p>
                             <input value={soDienThoai} className='form-control' name='soDienThoai' onChange={this.handleChangeInput}/>
-                            <p className='text-danger'>{this.state.errors.soDienThoai}</p>
+                            <p className='text-danger'>{this.props.sinhVienChinhSuaLoadLai.errors.soDienThoai}</p>
                         </div>
                         <div className='form-group'>
                             <p>Họ Tên</p>
                             <input value={hoTen} className='form-control' name='hoTen' onChange={this.handleChangeInput}/>
-                            <p className='text-danger'>{this.state.errors.hoTen}</p>
+                            <p className='text-danger'>{this.props.sinhVienChinhSuaLoadLai.errors.hoTen}</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div className='card-footer text-left'>
                 <button className='btn btn-success mr-2'>Thêm Sinh Viên</button>
+                <button type='button' className='btn btn-primary' onClick={()=> {
+                    const action = {
+                        type: 'CAP_NHAT',
+                        sinhVienCapNhat:this.props.sinhVienChinhSuaLoadLai.value
+                    }
+                    this.props.dispatch(action)
+                }}>Cập nhật</button>
             </div>
         </form>
         )
@@ -138,7 +148,8 @@ class Form extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        sinhVienChinhSua: state.baiTapFormReducer.sinhVienChinhSua
+        sinhVienChinhSua: state.baiTapFormReducer.sinhVienChinhSua,
+        sinhVienChinhSuaLoadLai: state.baiTapFormReducer.sinhVienChinhSuaLoadLai
     }
 }
 
--- src/redux/reducers/baiTapFormReducer.js
+++ src/redux/reducers/baiTapFormReducer.js
@@ -9,6 +9,20 @@ const stateDefault = {
         email:'nguyenvana@gmail.com',
         hoTen:'Nguyễn Văn A'
     },
+    sinhVienChinhSuaLoadLai: {
+        value: {
+            maSinhVien:'',
+            soDienThoai:'',
+            email:'',
+            hoTen:'',
+        },
+        errors : {
+            maSinhVien:'',
+            soDienThoai:'',
+            email:'',
+            hoTen:'',
+        }
+    }
 }
 export const baiTapFormReducer = (state = stateDefault,action) => {
     switch(action.type){
@@ -30,6 +44,23 @@ export const baiTapFormReducer = (state = stateDefault,action) => {
         }
         case 'HANDLE_CHANGE_INPUT': {
             state.sinhVienChinhSuaLoadLai = action.sinhVienChinhSuaLoadLai
+            return {...state}
+        }
+        case 'CAP_NHAT': {
+            const mangSinhVienCapNhatLai = [...state.mangSinhVien];
+            //tìm ra sinh viên cần cập nhật
+            // let sinhVienCapNhatLai = mangSinhVienCapNhatLai.find(sinhVienChinhSuaLoadLai=>sinhVienChinhSuaLoadLai.value.maSinhVien === action.sinhVienCapNhatLai.maSinhVien);
+             let sinhVienCapNhatLai = mangSinhVienCapNhatLai.find((sinhVien)=>{
+                 return sinhVien.maSinhVien === action.sinhVienCapNhat.maSinhVien
+             })
+            console.log(sinhVienCapNhatLai);
+            if(sinhVienCapNhatLai) {
+                sinhVienCapNhatLai.maSinhVien = action.sinhVienCapNhat.maSinhVien
+                sinhVienCapNhatLai.email = action.sinhVienCapNhat.email
+                sinhVienCapNhatLai.hoTen = action.sinhVienCapNhat.hoTen
+                sinhVienCapNhatLai.soDienThoai = action.sinhVienCapNhat.soDienThoai
+            }
+            state.mangSinhVien = mangSinhVienCapNhatLai;
             return {...state}
         }
         default: return state
